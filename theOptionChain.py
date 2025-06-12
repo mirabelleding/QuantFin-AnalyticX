b8bdba3bd6b257143546d60a89c69ad903d6e54e
mirabelleding <104223559+mirabelleding@users.noreply.github.com>
2025-06-11 20:19:58 -0400

    Update theOptionChain.py

--- theOptionChain.py
+++ theOptionChain.py
@@ -228,4 +228,4 @@ if ticker_input:
 else:
     st.info("Please enter a ticker symbol.")
 
-st.caption("Data cached for 1 hour to avoid repeated yFinance requests and rate limiting.")
+st.caption("*Data cached for 1 hour to avoid repeated yFinance requests and rate limiting.")
